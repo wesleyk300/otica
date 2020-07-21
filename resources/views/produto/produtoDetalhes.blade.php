d43b090c694eb2a1b84a345872af13764f82f13b
wesleyk300 <wesleycarlos300@hotmail.com>
2020-07-21 10:24:38 -0300

    Botão de editar
    Adicionado botão de editar para modelo-detalhado

--- resources/views/produto/produtoDetalhes.blade.php
+++ resources/views/produto/produtoDetalhes.blade.php
@@ -6,16 +6,20 @@
 
 <div class="text-center">
 
-           
-        COD Marca: </br>{{$list->idmarca}} </br></br>
-        COD Modelo: </br>{{$list->idmarca}} </br></br>
-        Nome Marca: </br>{{$list->nome_marca}} </br></br>
+
+        COD Marca: </br>{{$list->idmarca}} - {{$list->nome_marca}}</br></br>
+        COD Modelo: </br>{{$list->id_produto}} </br></br>
         Nome Modelo: </br>{{$list->modelo_produto}}</br></br>
         Cor: </br>{{$list->cor_produto}}</br></br>
         REF: </br>{{$list->ref_produto}}</br></br>
         Valor Entrada: </br>{{number_format($list->valor_entrada, 2, ',', '.')}}</br></br>
         Valor Saída: </br>{{number_format($list->valor_saida, 2, ',', '.')}}</br></br>
         Estoque:</br>{{$list->estoque}}</br></br>
+
+        <a href="/EditarModeloDetalhado/{{$list->id_produto}}">
+            <button class="btn btn-primary">Editar</button>
+        </a>
+        </br></br>
 
 </div>
 
